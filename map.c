30997d693f4d1e48d4b6460de2757f1c8cb40f6b
Edward Cragg <drq.11235@gmail.com>
2017-03-23 16:55:46 +0000

    calculate vector angle and magnitude

--- map.c
+++ map.c
@@ -1,28 +1,36 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
+#include <math.h>
 
 struct quirc_point {
 	int x;
 	int y;
 };
 
-struct quirc_point c[3] = {
+struct quirc_point cns[3] = {
 	{10, 10},
 	{100, 20},
 	{30, 120}
 };
 
+struct vector {
+	int i;			/* x component */
+	int j;			/* y component */
+	int m;			/* magnitude */
+};
+
+
 struct connection {
 	int p1;
 	int p2;
-	int m;		/* gradient */
-	int d;		/* displacement */
+	struct vector v;	/* connection vector */
 };
 
 unsigned char img[200 * 200];
 void write_img(void);
 void draw_points(void);
+void draw_matches(void);
 
 int main(void)
 {
@@ -30,33 +38,85 @@ int main(void)
 	printf("hello\n");
 
 	int i, j;
-	double m;
-	int d;
+	double d;
+	//int d;
 	int x, y;
 
 	struct connection connections[3 * 3 - 3];
 	int nc = 0;
 
+	struct vector a, b;
+	int dp;
+	double theta;
+
+	// optimise use of sqrt
+	// optimised acos() approximation
+
+	/* Calculate possible connection vectors */
 	for (i = 0; i < 3; i++) {
-		for (j = 0; j < 3; j++) if (i != j) {
-			x = c[j].x - c[i].x;
-			y = c[j].y - c[i].y;
+		for (j = 0; j < 3; j++) if (i != j)
+		{
+			struct connection c;
+			struct vector v;
 
-			printf("%d %d\n", x, y);
+			x = cns[j].x - cns[i].x;
+			y = cns[j].y - cns[i].y;
 
-			m = (double) y / x;
-			d = (y * y) + (x * x);
+			// approximate square root
+			d = sqrt((y * y) + (x * x));
+			printf("%d->%d d: %f x: %d y: %d\n", i, j, d, x, y);
+			//d = (y * y) + (x * x);
 
-			printf("%f %d\n", m, d);
+			c.p1 = i;
+			c.p2 = j;
 
-			struct connection c = {i, j, m, d};
+			v.i = x;
+			v.j = y;
+			v.m = d;
+			//v.i = x * x;
+			//v.j = y * y;
+
+			c.v = v;
+
 			connections[nc] = c;
 			nc++;
 		}
 	}
 
+	/* Evaluate every permutation, and score */
+	for (i = 0; i < nc; i++) {
+		for (j = 0; j < nc; j++) if (i != j)
+		{
+			struct connection c1 = connections[i];
+			struct connection c2 = connections[j];
+
+			/* vectors are joined at capstone B
+			 * and don't double back */
+			if (c1.p2 == c2.p1 && c1.p1 != c2.p2)
+			{
+				printf("%d->%d->%d\n", connections[i].p1,
+							connections[i].p2,
+							connections[j].p2);
+
+				a = c1.v;
+				b = c2.v;
+
+				/* calculate dot product */
+				dp = (a.i * b.i) + (a.j * b.j);
+
+				printf("dp: %d rhs: %f\n", dp, (double) dp / (a.m * b.m));
+				/* calculate angle */
+				// approximate
+				theta = acos((double) dp / (a.m * b.m));
+				printf("theta: %f\n", theta);
+
+				/* assign scores */
+			}
+		}
+	}
 
 	printf("done\n");
+	draw_matches();
 	write_img();
 }
 
@@ -66,13 +126,19 @@ void draw_points(void)
 
 	memset(img, 255, 200 * 200);
 	for (i = 0; i < 3; i++) {
-		img[c[i].x + (200 * c[i].y)] = 0;
-		img[c[i].x + (200 * c[i].y) + 1] = 0;
-		img[c[i].x + (200 * c[i].y) - 1] = 0;
-		img[c[i].x + (200 * (c[i].y - 1))] = 0;
-		img[c[i].x + (200 * (c[i].y + 1))] = 0;
-		printf("%d %d %d\n", i, c[i].x, c[i].y);
+		img[cns[i].x + (200 * cns[i].y)] = 0;
+		img[cns[i].x + (200 * cns[i].y) + 1] = 0;
+		img[cns[i].x + (200 * cns[i].y) - 1] = 0;
+		img[cns[i].x + (200 * (cns[i].y - 1))] = 0;
+		img[cns[i].x + (200 * (cns[i].y + 1))] = 0;
+		printf("%d %d %d\n", i, cns[i].x, cns[i].y);
 	}
+}
+
+void draw_matches(void)
+{
+
+
 }
 
 void write_img(void)

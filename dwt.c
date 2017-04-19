951fe4ef3db9b73751b56f81820885e3d61abfe6
Edward Cragg <drq.11235@gmail.com>
2017-04-19 09:21:09 +0100

    use row-based haar transform

--- dwt.c
+++ dwt.c
@@ -26,42 +26,46 @@ void discreteHaarWaveletTransform(unsigned char *input, int l, int *output)
 
 int main(int argc, char *argv[])
 {
-	unsigned char * in;
+	unsigned char *in;
 	int w, h;
+	int y;
 
 	printf("file: %s\n", argv[1]);
 	in = pgm_read(argv[1], &w, &h);
 	printf("width: %d height: %d\n", w, h);
 
 	int length, l;
-	length = l = w * h;
+	length = l = w;
 	printf("length: %d\n", length);
 	int log_length = 0b10;
 	while (length >>= 1) log_length <<= 1;
 	printf("log length: %d\n", log_length);
 
 	int *output;
-	output = malloc(log_length * sizeof(int));
+	output = malloc(log_length * h * sizeof(int));
 
-	unsigned char *new_in;
-	new_in = malloc(log_length * sizeof(unsigned char));
-	memset(new_in, 0, log_length);
-	memcpy(new_in, in, l);
+	char *row = malloc(log_length);
+	memset(row, 0, log_length);
 
-	discreteHaarWaveletTransform(new_in, log_length, output);
+	for (y = 0; y < h; y++)
+	{
+		memcpy(row, in + y * w, w);
+		discreteHaarWaveletTransform(row, log_length, output + y * log_length);
+
+	}
 
 	int i;
-	for (i = 0; i < log_length; i++)
-		printf("%d ", abs(output[i]));
+	//for (i = 0; i < log_length; i++)
+	//	printf("%d ", abs(output[i]));
 
-	unsigned char *out = malloc(l);
-	for (i = 0; i < l; i++)
+	unsigned char *out = malloc(log_length * h);
+	for (i = 0; i < log_length * h; i++)
 		out[i] = abs(output[i]);
 
 	//pgm_write("dwt.pgm", (int)sqrt(log_length), (int)sqrt(log_length), out);
-	pgm_write("dwt2.pgm", 330, 330, out);
+	pgm_write("dwt2.pgm", log_length, h, out);
 
 	free(output);
-	free(new_in);
+	free(row);
 	free(out);
 }
